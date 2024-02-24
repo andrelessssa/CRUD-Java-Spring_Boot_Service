bf9bc544c7dffc4092a346985d2f7ede324c975b
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2024-02-24 12:56:47 -0300

    :construction:   criado os métodos cadastrar e listar.
    :construction:   criado os métodos cadastrar e listar.

--- crud/src/main/java/com/example/crud/controller/CrudController.java
+++ crud/src/main/java/com/example/crud/controller/CrudController.java
@@ -1,9 +1,19 @@
 package com.example.crud.controller;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.crud.dto.CrudDto;
+import com.example.crud.model.CrudModel;
 import com.example.crud.service.CrudService;
 
 
@@ -14,7 +24,20 @@ public class CrudController {
     @Autowired
     private CrudService crudService;
 
+    @PostMapping
+    public ResponseEntity<CrudModel> criar(@RequestBody CrudDto crudDto){
+        CrudModel crudModel = crudService.criar(crudDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(crudModel);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CrudModel>> criar(){
+        List<CrudModel> lista = crudService.listar();
+        return ResponseEntity.status(HttpStatus.OK).body(lista);
+    }    
     
+
+
     
     
 }
--- crud/src/main/java/com/example/crud/service/CrudService.java
+++ crud/src/main/java/com/example/crud/service/CrudService.java
@@ -1,8 +1,13 @@
 package com.example.crud.service;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.crud.dto.CrudDto;
+import com.example.crud.model.CrudModel;
 import com.example.crud.repository.CrudRepository;
 
 @Service
@@ -10,5 +15,16 @@ public class CrudService {
 
     @Autowired
     private CrudRepository crudRepository;
+
+    public CrudModel criar(CrudDto crudDto){
+        CrudModel crudModel = new CrudModel();
+        BeanUtils.copyProperties(crudDto, crudModel);
+        return crudRepository.save(crudModel);
+
+    }
+    public List<CrudModel> listar(){
+        return crudRepository.findAll();
+    }
+    
     
 }
